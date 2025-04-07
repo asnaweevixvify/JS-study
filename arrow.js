20c90cec4fd7714906261621ad08b8d11ec8e282
asnaweevixvify <asnaweeezor@gmail.com>
2025-04-07 10:37:25 +0700

    arrow

---
+++ arrow.js
@@ -0,0 +1,10 @@
+// ชื่อฟังก์ชั่น = (parameter1,parameter2) => ค่าที่จะ return
+// หรือ 
+fullname=(fname,lname)=>fname+lname
+setAge=(age)=>"อายุ = "+age
+setAll=(name,email)=>{
+    return name+" "+email
+}
+console.log(fullname("asnawee","ezor"));
+console.log(setAge(17));
+console.log(setAll("asnawee","asnaweeezor@gmail.com"));
